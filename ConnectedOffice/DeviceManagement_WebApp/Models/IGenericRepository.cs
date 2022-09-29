0cd039a434986a201fb94dc42808acbc4a016180
FCM01 <faraimatyukira1@gmail.com>
2022-09-29 11:07:00 +0200

    comments

--- ConnectedOffice/DeviceManagement_WebApp/Models/IGenericRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Models/IGenericRepository.cs
@@ -5,12 +5,16 @@ using System.Linq.Expressions;
 public interface IGenericRepository<T> where T : class
 {
     T GetById(Guid? id);
+    //exists method
     bool Exists(Guid? id);
     IEnumerable<T> GetAll();
+    //create method 
     void Add(T entity);
+    //update method 
     void Update(T entity);
-    //this Getlist method is primarly used by the device controller 
+    //this Getlist method is primarly used by the device controller  i beleieve it should return the array require for Category and Zones
     IEnumerable<l> Getlist<l>() where l : class;
+    //delete method
     void Delete(T entity);
 }
 
